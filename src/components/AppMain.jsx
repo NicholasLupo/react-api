d291e45e71f8d2f870030be360bcfdbaa3b93ca2
NicholasLupo <nicholaslupo14@gmail.com>
2026-05-12 15:55:56 +0200

    fix fetch

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -9,11 +9,11 @@ export default function AppMain() {
 
     useEffect(() => {
 
-        fetch(male_actors)
-            .then(rep => rep.json())
-            .then(actor => {
-                setActors(actor)
-            })
+        /*         fetch(male_actors)
+                    .then(rep => rep.json())
+                    .then(actor => {
+                        setActors(actor)
+                    }) */
 
         fetch(female_actresses)
             .then(rep => rep.json())
@@ -27,18 +27,27 @@ export default function AppMain() {
     return (
         <main>
             <div className="container">
-                <h1> Every actors and actresses </h1>
-                <div className="row row-cols-3">
-                    <div className="col">
-                        <div className="card" key={actors.id}>
-                            <img src={actors.image} alt="" />
-                            <div className="card-body">
-                                <p> Name: <strong>{actors.name}</strong> </p>
-                                <p> Nationality: <strong>{actors.nationality}</strong> </p>
-                                <p> Birthday: <strong>{actors.birth_year}</strong> </p>
+                <h1 className="text-center p-3"> Every actors and actresses </h1>
+                <div className="row row-cols-3 g-3">
+                    {
+                        actors.map(actor => (
+
+                            <div className="col-12 col-sm-6 col-md-4">
+                                <div className="card" key={actor.id}>
+                                    <img src={actor.image} alt="" />
+                                    <div className="card-body">
+                                        <p><strong> Name: </strong>  {actor.name} </p>
+                                        <p><strong> Nationality: </strong>  {actor.nationality} </p>
+                                        <p><strong> Birthday: </strong>  {actor.birth_year} </p>
+                                        <p><strong> Death: </strong>  {actor.death_year || 'Alive'} </p>
+                                        <p><strong> Awards: </strong>  {actor.awards[0]} , {actor.awards[1]} </p>
+                                        <p><strong> Biography: </strong>{actor.biography}</p>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
+
+                        ))
+                    }
                 </div>
             </div>
         </main>
