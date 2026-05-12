a20ace233786f387e30af536f3eba9a80f0b6d48
NicholasLupo <nicholaslupo14@gmail.com>
2026-05-12 15:21:08 +0200

    fetch api

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react"
+
 export default function AppMain() {
+
+    const [actors, setActors] = useState([])
 
     const male_actors = 'https://lanciweb.github.io/demo/api/actors/'
     const female_actresses = 'https://lanciweb.github.io/demo/api/actresses/'
+
+    fetch(male_actors)
+        .then(rep => rep.json())
+        .then(actor => {
+            setActors(actor)
+        })
+
+    fetch(female_actresses)
+        .then(rep => rep.json())
+        .then(actor => {
+            setActors(actor)
+        })
 
     return (
         <main>
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import * as bootstrap from 'bootstrap'
-import * as bootstrap from 'bootstrap-icons'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
