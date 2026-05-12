e1de40fb89b71dcc20d3dde7c58084a67fb64c4b
NicholasLupo <nicholaslupo14@gmail.com>
2026-05-12 15:15:32 +0200

    import url api

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,7 +1,17 @@
 export default function AppMain() {
 
+    const male_actors = 'https://lanciweb.github.io/demo/api/actors/'
+    const female_actresses = 'https://lanciweb.github.io/demo/api/actresses/'
 
     return (
-        <main></main>
+        <main>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
